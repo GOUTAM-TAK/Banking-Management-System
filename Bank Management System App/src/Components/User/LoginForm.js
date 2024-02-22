9c97923bfb557f8f9bf3eff7e609f58ce10e0897
GovindaTak <govindatak19@gmail.com>
2024-02-22 11:04:19 +0530

    User components

---
+++ Bank Management System App/src/Components/User/LoginForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import BankServices from '../../BankService/BankServices';
+import { HttpStatusCode } from 'axios';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission here, like sending the email and password to your server for authentication
+    console.log('Submitted:', { email, password });
+    BankServices.userLogin({email:email,password:password}).then((obj)=>displayMassage(obj));
+  }
+  function displayMassage(obj) {
+    const message = document.getElementById("message");
+    var bool=obj.status===HttpStatusCode.Ok ? true:false;
+    if (bool) {
+      message.textContent = "Successfully login !";
+      message.style.color = "green";
+    if( obj.data.role==='USER')
+      history.push("/user/welcome");
+    } else {
+      message.textContent = "Problem with Server !";
+      message.style.color = "red";
+    }
+  }
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header">Login</div>
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <h1 id='message'></h1>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary btn-block">Login</button>
+              </form>
+          <Link to="/addUser"><button type="button" className="btn btn-primary btn-block">Sign Up</button></Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
+// @NotBlank(message = "Email can't be blank")
+// @Email(message = "Invalid email format")
+// private String email;
+// //	@NotBlank
+//  @Pattern(regexp = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/")
+// @Length(min = 3,max=20,message = "Invalid password length")
+// private String password;
